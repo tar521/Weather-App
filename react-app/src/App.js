87e4138ac08343264faf7e68add62a56e8c19563
Tristram Reed <tar123@txstate.edu>
2023-09-12 14:00:23 -0500

    login page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,91 @@
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "./lib/contextLib";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Routes from "./Routes";
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
-  return (
-    <div>Hello World</div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  const nav = useNavigate();
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await loggedIn();
+      userHasAuthenticated(true);
+    } catch (e) {
+      // if (e !== "No current user") {
+      //   alert(e);
+      // }
+    }
+  
+    setIsAuthenticating(false);
+  }
+
+  async function loggedIn() {
+    if (!token) {
+      throw new Error("NOT LOGGED IN");
+    }
+
+  }
+  
+
+  function handleLogout() {
+    userHasAuthenticated(false);
+    setToken("");
+    nav("/login");
+    //setIsAuthenticating(true);
+  }
+
+  sessionStorage.clear();
+
+  return(
+    !isAuthenticating && (
+      <>
+        <Navbar collapseOnSelect bg="secondary opacity-75" data-bs-theme="dark" expand="md" className="mb-3">
+          <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              Weather App
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+            {isAuthenticated ? (
+              <>
+                <LinkContainer to="/settings">
+                  <Nav.Link>Settings</Nav.Link>
+                </LinkContainer>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated, token, setToken }}>
+          <Routes />
+        </AppContext.Provider>
+    </>
+    )
   );
 }
 
